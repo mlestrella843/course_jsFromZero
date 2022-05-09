7a9c5f0ddbcba2fd0077991a5cc315b547fb4278
Maria <98559654+mlestrella843@users.noreply.github.com>
2022-05-09 08:28:11 -0600

    Evento con videos

---
+++ event_dom.js
@@ -0,0 +1,64 @@
+//DomContentloaded, indica, oye espera a que carge todo y luego
+//ejecutas este script
+//Para hacer la prueba pondre el script arriba DEL  BODY en la pagina html
+//o en el header
+
+//El evento load ocurre despues del DOMContentLoaded, ya que es mas rapido
+
+/*
+addEventListener('DOMContentLoaded', () => {
+
+const form = document.getElementById('form')
+const rememberPassword = document.getElementById('remember-password')
+
+form.addEventListener('submit', () => {
+    console.log('Ha enviado el formulario')
+})
+
+
+rememberPassword.addEventListener('change', e => {
+   // console.log(e)
+    if(e.target.checked){
+        console.log('El usuario quiere recordar su contraseña')
+    } else{
+        console.log('El usuario no quiere recordar su contraseña')
+          }
+
+   })
+
+})
+
+*/
+
+//Evento SCROLL 
+
+/*
+addEventListener('scroll', e => {
+    console.log(scrollX, scrollY)
+})
+
+
+//Evento RESIZE, toma los valores cuando se mueve la pantalla
+
+addEventListener('resize', e => {
+    console.log(innerWidth, innerHeight)
+    console.log(outerWidth, outerHeight)
+})
+*/
+
+//Eventos MULTIMEDIA
+//los eventos tambien pueden ejecutarse, no solo escucharse con addevenlistener
+//por ejemplo en video.play estoy ejecutando directamente el evento de play.
+
+const video = document.getElementById('video')
+const playButton = document.getElementById('play')
+const pauseButton = document.getElementById('pause')
+
+playButton.addEventListener('click', () => {
+    video.play()
+})
+
+pauseButton.addEventListener('click', () => {
+    video.pause()
+})
+
